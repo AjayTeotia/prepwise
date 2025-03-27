c955db5cc2c94a6d330c1d35b6673e32f6ddb732
AjayTeotia <teotiaajay20@gmail.com>
2025-03-27 12:28:59 +0530

    auth-page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,5 @@
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-    return (
-        <div
-            className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8"
-        >
-            {children}
-        </div>
-    );
+    return <div className="auth-layout">{children}</div>;
 }
