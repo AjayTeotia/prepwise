ffca618ffb8c67ab63ec47ec90f6e89fcee24f38
AjayTeotia <teotiaajay20@gmail.com>
2025-03-28 13:26:17 +0530

    interview-generation-ui

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,20 @@
+import Agent from "@/components/_global/Agent";
+import { getCurrentUser } from "@/lib/action/auth.action";
+
+export default async function Interview() {
+    const user = await getCurrentUser();
+
+
+    return (
+        <>
+            <h3>Interview generation</h3>
+
+            <Agent
+                userName={user?.name || ""}
+                userId={user?.id || ""}
+                profileImage={user?.profileURL || ""}
+                type="generate"
+            />
+        </>
+    );
+}
