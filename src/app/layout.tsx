c18c14f91cce84ba21334a5956ec0b310e65ff96
AjayTeotia <teotiaajay20@gmail.com>
2025-03-31 20:18:43 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const monaSans = Mona_Sans({
   variable: "--font-mono-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
         {children}
 
         <Toaster />
+
+        <Analytics />
       </body>
     </html>
   );
